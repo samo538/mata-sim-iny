8cfa3ee1afb9d232afe4416d83e45589a9c473c9
Samuel Luptak <xluptas00@stud.fit.vutbr.cz>
2024-09-17 15:09:20 +0200

    Bench: Sources

---
+++ src/utils/config.hh
@@ -0,0 +1,11 @@
+#ifndef LIBMATA_PERFTEST_CONFIG_H
+#define LIBMATA_PERFTEST_CONFIG_H
+
+#include <string>
+
+namespace mata::PerformanceTesting {
+    const std::string AUTOMATA_DIR = "/home/luptak/School_Projects/IP1/mata/mata/tests-integration/automata";
+    const std::string BENCHMARK_DIR = "/home/luptak/School_Projects/IP1/mata/mata/tests-integration/benchmark";
+}
+
+#endif //LIBMATA_PERFTEST_CONFIG_H
